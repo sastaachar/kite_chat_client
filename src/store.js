6c88e27ecaf5db8ab9399a41e192f7b289a8f97e
sastaachar <justinjohnmathew@karunya.edu.in>
2020-05-30 14:07:53 +0530

    finally fixed the redux bug

--- src/store.js
+++ src/store.js
@@ -7,12 +7,14 @@ const initialState = {};
 const middleware = [thunk];
 
 const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-        applyMiddleware(...middleware),
-        (process.env.NODE_ENV === 'development') ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null
-    )
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
+  )
 );
 
 export default store;
