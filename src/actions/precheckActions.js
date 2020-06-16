eb44731952693453b81e8a88a8dc9aaa92a0aa4d
sastaachar <justinjohnmathew@karunya.edu.in>
2020-06-16 23:47:32 +0530

    typo fixed 2

--- src/actions/precheckActions.js
+++ src/actions/precheckActions.js
@@ -15,7 +15,7 @@ export const preCheck = () => (dispatch) => {
   let headers = new Headers();
   //no need for these stupid header
   headers.append("Content-Type", "application/json");
-  headers.append("Origin", CLIENT_URL);
+  //headers.append("Origin", CLIENT_URL);
   headers.append("Access-Control-Allow-Credentials", "true");
   let responseOK;
   fetch(`${SERVER_URL}/users`, {
